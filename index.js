7afbaf5071d2b645aeb5182888f94241c0d7da59
KrimeKapitol <143522221+KrimeKapitol@users.noreply.github.com>
2023-08-29 10:23:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 app.all('/fivem/api', (req, res) => {
     const _data = "750066aa-197a-4d08-aedc-2f92562982d2";
     const User = req.socket.remoteAddress; 
+    console.log("[API]:", _data, "User:", User);
      res.json(_data);
 })
 app.listen(process.env.PORT || 30422)
